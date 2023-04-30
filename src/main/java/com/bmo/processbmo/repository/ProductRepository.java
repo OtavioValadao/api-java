1fd5f36615582840ec254127d2f32aa982b5c85a
Marlon Jerold Moura Martins <jeroldmarlon12@gmail.com>
2023-04-30 14:56:11 -0300

    Reafatoração de ProductController e configuração para subida

--- src/main/java/com/bmo/processbmo/repository/ProductRepository.java
+++ src/main/java/com/bmo/processbmo/repository/ProductRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     void deleteById(Long id);
-    void merge(Product product);
+    //void merge(Product product);
 }
--- src/main/java/com/bmo/processbmo/view/controller/ProductController.java
+++ src/main/java/com/bmo/processbmo/view/controller/ProductController.java
@@ -2,11 +2,11 @@ package com.bmo.processbmo.view.controller;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,10 +16,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.bmo.processbmo.model.Product;
 import com.bmo.processbmo.services.ProductService;
 import com.bmo.processbmo.shared.ProdutDTO;
+import com.bmo.processbmo.view.Model.ProductRequest;
 import com.bmo.processbmo.view.Model.ProductResponse;
 
 @RestController
@@ -30,7 +29,7 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping
-    public List<ProductResponse> getAll(){
+    public ResponseEntity<List<ProductResponse>> getAll(){
         List<ProdutDTO> productDTOs = productService.getAll();
 
         ModelMapper mapper = new ModelMapper();
@@ -41,27 +40,54 @@ public class ProductController {
                 ProductResponse.class))
             .collect(Collectors.toList());
 
-        return productResponse;
+        return new ResponseEntity<>(productResponse, HttpStatus.OK);
     }
 
     @PostMapping
-    public Product addProduct(@RequestBody Product product){
-        return productService.add(product);
+    public ResponseEntity<ProductResponse> addProduct(@RequestBody ProductRequest productRequest){
+        ModelMapper mapper = new ModelMapper();
+
+        ProdutDTO produtDTO = mapper.map(productRequest, ProdutDTO.class);
+
+        produtDTO =  productService.add(produtDTO);
+
+        return new ResponseEntity<>(mapper.map(produtDTO, ProductResponse.class), HttpStatus.CREATED );
+
+        
     }
 
+    /**
+     * @param id
+     * @return
+     */
     @GetMapping("/{id}")
-    public Optional<Product> getbyId(@PathVariable Integer id){
-        return productService.getbyId(id);
+    public ResponseEntity<Optional<ProductResponse>> getbyId(@PathVariable Integer id){
+        Optional<ProdutDTO> productDTO =  productService.getbyId(id);
+
+        ProductResponse productResponse = new ModelMapper().map(productDTO.get(), ProductResponse.class);
+
+        return new ResponseEntity<>(Optional.of(productResponse), HttpStatus.OK);
+
     }
 
     @DeleteMapping("/{id}")
-    public void deleteProduct(@PathVariable Integer id){
+    public ResponseEntity<?> deleteProduct(@PathVariable Integer id){
         productService.deleteProduct(id);
+        
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
     
     @PutMapping("/{id}")
     @Transactional
-    public Product updateProduct(@RequestBody Product product, @PathVariable Integer id){
-        return productService.update(id, product);
+    public ResponseEntity<ProductResponse> updateProduct(@RequestBody ProductRequest productRequest, @PathVariable Integer id){
+        ModelMapper mapper = new ModelMapper();
+        ProdutDTO produtDTO = mapper.map(productRequest, ProdutDTO.class);
+
+        produtDTO = productService.update(id, produtDTO);
+
+        return new ResponseEntity<>(
+            mapper.map(produtDTO, ProductResponse.class),
+            HttpStatus.OK
+        );
     }
 }
